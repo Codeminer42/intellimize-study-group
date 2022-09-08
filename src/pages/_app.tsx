39883620003312fc3b8774a6f56791247a8cdfe8
Isaac Alencar <isaac.alencar@codeminer42.com>
2022-09-08 18:02:44 +0000

    Setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app';
+
+import { theme } from '../ui/styles/theme';
+import { GlobalStyles } from '../ui/styles/global';
+
+import { ThemeProvider } from 'styled-components';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
