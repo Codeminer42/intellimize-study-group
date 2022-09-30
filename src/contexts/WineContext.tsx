2263f0cc55114515ffdff7b6f0f4439d10de8150
Isaac Alencar <alencarisaac3@gmail.com>
2022-09-30 17:08:07 -0300

    create WineContext

---
+++ src/contexts/WineContext.tsx
@@ -0,0 +1,91 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useCallback, useContext, useMemo, useState } from 'react';
+
+export type Wine = {
+  id: number;
+  name: string;
+  type: string;
+  year: number;
+  price: number;
+  location: string;
+  vineyardId: number;
+  wineryId: number;
+  imgUrl: string;
+};
+
+type Wineries = {
+  id: number;
+  name: string;
+  details: string;
+};
+
+type Vineyard = {
+  id: number;
+  location: string;
+  details: string;
+};
+
+type WineData = {
+  wines: Wine[] | undefined;
+  setWines: Dispatch<SetStateAction<Wine[] | undefined>>;
+};
+
+export const WineContext = createContext<WineData | null>(null);
+
+const fakeWineData: Wine[] = [
+  {
+    id: 1,
+    name: 'Wine test',
+    type: 'Tint',
+    year: 1967,
+    price: 120.0,
+    location: 'Sicilia, Italia',
+    vineyardId: 1,
+    wineryId: 2,
+    imgUrl: '/assets/wine.png',
+  },
+  {
+    id: 2,
+    name: 'Wine test',
+    type: 'Tint',
+    year: 1967,
+    price: 120.0,
+    location: 'Sicilia, Italia',
+    vineyardId: 1,
+    wineryId: 2,
+    imgUrl: '/assets/wine.png',
+  },
+  {
+    id: 3,
+    name: 'Wine test',
+    type: 'Tint',
+    year: 1967,
+    price: 120.0,
+    location: 'Sicilia, Italia',
+    vineyardId: 1,
+    wineryId: 2,
+    imgUrl: '/assets/wine.png',
+  },
+];
+
+export const WineDataProvider = ({ children }: { children: ReactNode }) => {
+  const [wines, setWines] = useState<Wine[] | undefined>(fakeWineData);
+
+  const value = {
+    wines,
+    setWines,
+  };
+
+  return <WineContext.Provider value={value}>{children}</WineContext.Provider>;
+};
+
+const useWines = () => {
+  const winesContext = useContext(WineContext);
+
+  if (!winesContext) {
+    throw new Error('useWines hook needs to be inside a WinesContext');
+  }
+
+  return winesContext;
+};
+
+export { useWines };
