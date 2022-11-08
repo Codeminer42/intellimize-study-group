18fd4ce56744249930546475ea4c6e95d762df0e
Isaias <isaigrec@gmail.com>
2022-11-08 16:11:02 -0300

    Finish styles

--- idega/src/ui/components/WineryForm.tsx
+++ idega/src/ui/components/WineryForm.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { Header } from './Header';
+import { Button, ButtonVariants } from './Button';
 import { useAddWineryForm } from '../hooks/useAddWineryForm';
 
 const WineryForm = () => {
@@ -11,35 +12,42 @@ const WineryForm = () => {
       <Header />
 
       <Container>
-        <h1>Add Winery</h1>
+        <FormTitle>Add Winery</FormTitle>
 
         <Form onSubmit={handleSubmit} noValidate>
-          <label htmlFor='wineryName'>Winery Name</label>
-          <input
-            id='wineryName'
-            name='name'
-            placeholder='your winery name'
-            value={winery.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
+          <FormSection>
+            <label htmlFor='wineryName'>Winery Name</label>
+            <input
+              id='wineryName'
+              name='name'
+              placeholder='new winery name'
+              value={winery.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
 
-          {isInvalid.name && <ErrorField className='form-error field-error--name'>{errors.name}</ErrorField>}
+            {isInvalid.name && <ErrorField>{errors.name}</ErrorField>}
+          </FormSection>
 
-          <label htmlFor='wineryLocation'>Winery Location</label>
-          <input
-            id='wineryLocation'
-            name='location'
-            placeholder='winery location'
-            value={winery.location}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {isInvalid.location && (
-            <ErrorField className='form-error field-error--location'>{errors.location}</ErrorField>
-          )}
+          <FormSection>
+            <label htmlFor='wineryLocation'>Winery Location</label>
+            <input
+              id='wineryLocation'
+              name='location'
+              placeholder='winery location'
+              value={winery.location}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
 
-          <button type='submit'>Submit</button>
+            {isInvalid.location && <ErrorField>{errors.location}</ErrorField>}
+          </FormSection>
+
+          <ButtonSection>
+            <Button variant='green' type='submit'>
+              Submit
+            </Button>
+          </ButtonSection>
         </Form>
       </Container>
     </>
@@ -50,12 +58,45 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
+  margin-top: 15px;
+`;
+
+const FormTitle = styled.h1`
+  font-size: 2em;
 `;
 
 const Form = styled.form`
   display: flex;
+  width: 100%;
   align-items: center;
   flex-direction: column;
+  justify-content: space-between;
+  margin-top: 15px;
+`;
+
+const FormSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  margin-bottom: 5px;
+
+  label,
+  span {
+    font-size: 1.1rem;
+  }
+
+  input {
+    height: 35px;
+  }
+`;
+
+const ButtonSection = styled.div`
+  button {
+    width: 100px;
+    height: 40px;
+
+    font-size: 18px;
+  }
 `;
 
 const ErrorField = styled.span`
